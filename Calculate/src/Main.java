61757676e2c31353c78c43a135c68cf444cb52bb
3119005471 <114514@qq.com>
2021-10-18 13:04:05 +0800

    四则——初版

---
+++ Calculate/src/Main.java
@@ -0,0 +1,36 @@
+
+
+import auto.GenerationOfRandomArithmetic;
+import calculation.CalculationOfRandomArithmetic;
+import grade.Grade;
+import outputfile.OutputAnser;
+import outputfile.OutputArithmetic;
+import util.BinaryTree;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        GenerationOfRandomArithmetic generation = new GenerationOfRandomArithmetic();
+        CalculationOfRandomArithmetic calculation = new CalculationOfRandomArithmetic();
+        BinaryTree<String> binaryTree;
+        String answer ,arithmetic;
+
+        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+            binaryTree = new BinaryTree<>();
+            arithmetic =generation.generateArithmetic(binaryTree,Integer.parseInt(args[3]));
+            answer=calculation.calculationOfRandomArithmetic(binaryTree.postorderTraversal());
+            if(answer==null){
+                i--;
+                continue;
+            }else {
+                OutputArithmetic.outputArithmetic(arithmetic);
+                OutputAnser.outputAnswers(answer);
+            }
+
+        }
+        OutputArithmetic.close();
+        OutputAnser.close();
+
+    }
+}
